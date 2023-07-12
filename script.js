09d69ec5ab7fdb86e55e2c51607f15e13ee77682
plzusername <MohamedFaris2210@gmail.com>
2023-07-12 16:21:30 +0300

    Add a game function to run the round 5 times

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let Userchoice=prompt('Rock, paper, or scissors?')
+let UserChoice;
+let computer_score=0;
+let player_score=0;
 function getComputerChoice(){
     let random=Math.floor(Math.random()*3)+1;
     let choice;
@@ -16,49 +18,59 @@ function getComputerChoice(){
     }
     return choice;
 }
-function playRound(){
+function playRound(compChoice){
+    let UserChoice=prompt('Rock, paper, or scissors?')
     let roundResult;
-    let computer_score;
-    let player_score;
-    if(getComputerChoice().toLowerCase()=='rock'&&Userchoice.toLowerCase()=='rock'){
+    if(compChoice.toLowerCase()=='rock'&&UserChoice.toLowerCase()=='rock'){
         roundResult='Rock vs Rock? that is a tie!'
     }
-    else if(getComputerChoice().toLowerCase()=='paper'&&Userchoice.toLowerCase()=='paper'){
+    else if(compChoice.toLowerCase()=='paper'&&UserChoice.toLowerCase()=='paper'){
         roundResult='Paper vs Paper? that is a tie!'
     }
-    else if(getComputerChoice().toLowerCase()=='scissors'&&Userchoice.toLowerCase()=='scissors'){
+    else if(compChoice.toLowerCase()=='scissors'&&UserChoice.toLowerCase()=='scissors'){
         roundResult='scissors vs scissors? that is a tie!'
     }
-    else if(getComputerChoice().toLowerCase()=='rock'&&Userchoice.toLowerCase()=='paper'){
+    else if(compChoice.toLowerCase()=='rock'&&UserChoice.toLowerCase()=='paper'){
         roundResult='The user wins! paper covers rock!'
         player_score+=1;
     }
-    else if(getComputerChoice().toLowerCase()=='rock'&&Userchoice.toLowerCase()=='scissors'){
+    else if(compChoice.toLowerCase()=='rock'&&UserChoice.toLowerCase()=='scissors'){
         roundResult='The computer wins! rock breaks scissors!'
         computer_score+=1;
     }
-    else if(getComputerChoice().toLowerCase()=='paper'&&Userchoice.toLowerCase()=='rock'){
+    else if(compChoice.toLowerCase()=='paper'&&UserChoice.toLowerCase()=='rock'){
         roundResult='The computer wins! paper covers  rock!'
         computer_score+=1;
 
     }
-    else if(getComputerChoice().toLowerCase()=='paper'&&Userchoice.toLowerCase()=='scissors'){
+    else if(compChoice.toLowerCase()=='paper'&&UserChoice.toLowerCase()=='scissors'){
         roundResult='The user wins! scissors cuts paper!'
         player_score+=1;
 
     }
-    else if(getComputerChoice().toLowerCase()=='scissors'&&Userchoice.toLowerCase()=='paper'){
+    else if(compChoice.toLowerCase()=='scissors'&&UserChoice.toLowerCase()=='paper'){
         roundResult='The computer wins! scissors cuts paper!'
         computer_score+=1;
 
     }
-    else if(getComputerChoice().toLowerCase()=='scissors'&&Userchoice.toLowerCase()=='rock'){
+    else if(compChoice.toLowerCase()=='scissors'&&UserChoice.toLowerCase()=='rock'){
         roundResult='The user wins! rock breaks scissors!'
         player_score+=1;
 
     }
-    else{
-        return null;
-    }
+
     return roundResult
 }
+function game(){
+    for(let i=0;i<5;i++){
+        const compChoice=getComputerChoice();
+        console.log(playRound(compChoice))
+    }
+    if(player_score==5){
+        console.log('You win!')
+    }
+    if(computer_score==5){
+        console.log('You Lose!')
+    }
+}
+game()
