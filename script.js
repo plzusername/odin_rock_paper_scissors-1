e8855b986b41f7ab2dbed35a6cca777a207d66bc
plzusername <MohamedFaris2210@gmail.com>
2023-07-12 14:29:32 +0300

    Add function to play round
    Add a function that checks a group of conditionals regarding the users
    choice, increasing the user score by one if he wins, and increasing the
    computer score by one if it wins

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let Userchoice=prompt('Rock, paper, or scissors?')
 function getComputerChoice(){
     let random=Math.floor(Math.random()*3)+1;
     let choice;
@@ -11,7 +12,53 @@ function getComputerChoice(){
         choice='scissors';
     }
     else{
-
+        return null
     }
     return choice;
+}
+function playRound(){
+    let roundResult;
+    let computer_score;
+    let player_score;
+    if(getComputerChoice()=='rock'&&Userchoice=='rock'){
+        roundResult='Rock vs Rock? that is a tie!'
+    }
+    else if(getComputerChoice()=='paper'&&Userchoice=='paper'){
+        roundResult='Paper vs Paper? that is a tie!'
+    }
+    else if(getComputerChoice()=='scissors'&&Userchoice=='scissors'){
+        roundResult='scissors vs scissors? that is a tie!'
+    }
+    else if(getComputerChoice()=='rock'&&Userchoice=='paper'){
+        roundResult='The user wins! paper covers rock!'
+        player_score+=1;
+    }
+    else if(getComputerChoice()=='rock'&&Userchoice=='scissors'){
+        roundResult='The computer wins! rock breaks scissors!'
+        computer_score+=1;
+    }
+    else if(getComputerChoice()=='paper'&&Userchoice=='rock'){
+        roundResult='The computer wins! paper covers  rock!'
+        computer_score+=1;
+
+    }
+    else if(getComputerChoice()=='paper'&&Userchoice=='scissors'){
+        roundResult='The user wins! scissors cuts paper!'
+        player_score+=1;
+
+    }
+    else if(getComputerChoice()=='scissors'&&Userchoice=='paper'){
+        roundResult='The computer wins! scissors cuts paper!'
+        computer_score+=1;
+
+    }
+    else if(getComputerChoice()=='scissors'&&Userchoice=='rock'){
+        roundResult='The user wins! rock breaks scissors!'
+        player_score+=1;
+
+    }
+    else{
+        return null;
+    }
+    return roundResult
 }
